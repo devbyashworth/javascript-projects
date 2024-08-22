8c75a7b2e8356ff0ffcd4b97b70896e91e275611
Ashworth <ashworthsakara13@gmail.com>
2024-08-22 14:37:48 +0200

    Two New Projects Added

---
+++ projects/dice-roller/script.js
@@ -0,0 +1,15 @@
+const rollDice = () => {
+  const diceImages = document.getElementById("diceImages");
+  const diceResults = document.getElementById("diceResults");
+  const numOfDice = document.getElementById("numOfDice").value;
+  const values = [];
+  const images = [];
+
+  for (let i = 0; i < numOfDice; i++) {
+    const value = Math.floor(Math.random() * 6) + 1;
+    values.push(value);
+    images.push(`<img src="dice_images/${value}.png" alt="Dice ${value}">`);
+  }
+  diceResults.textContent = `Dice: ${values.join(", ")}`;
+  diceImages.innerHTML = images.join("");
+};
