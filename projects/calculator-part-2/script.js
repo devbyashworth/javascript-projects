e9d0b917147324e4cb093931789d4d619507f6f9
Ashworth <78765767+Ashworth836@users.noreply.github.com>
2024-08-22 15:17:06 +0200

    Update script.js

--- projects/calculator-part-2/script.js
+++ projects/calculator-part-2/script.js
@@ -1,25 +1,29 @@
-const display =  document.querySelector(".display");
-const buttons =  document.querySelectorAll("button");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 const specialChars = ["%", "/", "*", "+", "-", "="];
 let output = "";
 
 const calculate = (btnValue) => {
-    display.focus();
-     if (btnValue === "AC") {
-        output = "";
-    } else if (btnValue === "DEL") {
-        output = output.toString().slice(0, -1);
-    } else if (btnValue === "=" && output !== "") {
-        output = eval(output.replace("%", "/100"));
-    } else {
-        if (output === "" && specialChars.includes(btnValue)) return;
-        output += btnValue;
+  display.focus();
+  if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else if (btnValue === "=" && output !== "") {
+    try {
+      output = eval(output.replace("%", "/100"));
+    } catch {
+      output = "Error";
     }
-    display.value = output;
-} 
+  } else {
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
 
 buttons.forEach((button) => {
-    button.addEventListener("click", (evt) => {
-        calculate(evt.target.dataset.value)
-    });
+  button.addEventListener("click", (evt) => {
+    calculate(evt.target.dataset.value);
+  });
 });
