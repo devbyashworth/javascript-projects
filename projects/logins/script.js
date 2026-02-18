f03ff4ee4df707277ed901f17dafaed5224bfa6a
Ashworth <ashworthsakara13@gmail.com>
2026-02-18 03:04:13 +0200

    Added a Logins Project

---
+++ projects/logins/script.js
@@ -0,0 +1,23 @@
+const navButtons = document.querySelectorAll(".card .nav button");
+const cardForms = document.querySelector(".card .forms");
+const activeBar = document.querySelector(".active-bar");
+const cardHeroInner = document.querySelector(".card .hero-inner");
+
+const selectView = (view) => {
+  for (const button of navButtons) {
+    button.classList.remove("active");
+
+    if (button.classList.contains(view)) {
+      button.classList.add("active");
+    }
+  }
+  if (view === "signin") {
+    cardForms.style.top = "0";
+    activeBar.style.top = "33.33%";
+    cardHeroInner.style.top = "0";
+  } else {
+    cardForms.style.top = "-100%";
+    activeBar.style.top = "66.66%";
+    cardHeroInner.style.top = "-100%";
+  }
+};
